0b94eec278532ec3a59e93aafde671e09088478c
iddixx <ondarmurad2009@gmail.com>
2025-08-01 14:12:02 +0300

    (dmenu) layout didn't change, so i removed layout changing

--- run_dmenu.d
+++ run_dmenu.d
@@ -6,7 +6,8 @@ import std.stdio;
 // i'm lazy to change layout by hand, after launching dmenu
 void main()
 {
-    executeShell("setxkbmap us -variant colemak_dh -option grp:shifts_toggle -option caps:capslock");
+    /*executeShell("setxkbmap us -variant colemak_dh -option grp:shifts_toggle -option caps:capslock");*/
     executeShell("dmenu_run -fn 'Iosevka Fixed SS14 SemiBold:pixelsize=25:antialias=true' -nb '#61325C' -nf '#F7A0CB' -sb '#F9B7DD' -sf '#A853BB'");
-    executeShell("setxkbmap us,ru,ua -variant colemak_dh,diktor,diktor -option grp:shifts_toggle -option caps:capslock");
+    /*executeShell("setxkbmap us,ru,ua -variant colemak_dh,diktor,diktor -option grp:shifts_toggle -option caps:capslock");*/
+    // didnt work ;(
 }
