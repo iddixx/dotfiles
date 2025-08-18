11a35019ec59b71f18837b341962d2ad02503d58
iddixx <ondarmurad2009@gmail.com>
2025-08-18 22:47:22 +0300

    Remade run_dmenu.d script

--- run_dmenu.d
+++ run_dmenu.d
@@ -1,13 +1,25 @@
 #!/bin/rdmd
 import std.process;
+import std.getopt;
 import std.stdio;
+import std.uni;
 
-// i wrote that script just because my laziness
-// i'm lazy to change layout by hand, after launching dmenu
-void main()
+void main(string[] args)
 {
-    /*executeShell("setxkbmap us -variant colemak_dh -option grp:shifts_toggle -option caps:capslock");*/
-    executeShell("dmenu_run -fn 'Iosevka Fixed SS14 SemiBold:pixelsize=25:antialias=true' -nb '#61325C' -nf '#F7A0CB' -sb '#F9B7DD' -sf '#A853BB'");
-    /*executeShell("setxkbmap us,ru,ua -variant colemak_dh,diktor,diktor -option grp:shifts_toggle -option caps:capslock");*/
-    // didnt work ;(
+    string cmd = "-fn 'Iosevka Fixed SS14 SemiBold:pixelsize=25:antialias=true' ";
+    bool clipmenu = false;
+    string theme = "";
+
+    auto flags = getopt(args,
+            "clipmenu", &clipmenu,
+            "theme", &theme);
+
+    if(toLower(theme) == "ram")
+        cmd ~= "-nb '#61325C' -nf '#F7A0CB' -sb '#F9B7DD' -sf '#A853BB'";
+    else if(toLower(theme) == "xmonad")
+        cmd ~= "-nb '#000000' -nf '#FFFFFF' -sb '#6881b5' -sf '#000000'";
+    if(clipmenu)
+        executeShell("clipmenu -b " ~ cmd);
+    else
+        executeShell("dmenu_run " ~ cmd);
 }
