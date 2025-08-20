ffe6062af713d3777c76a1bd980d8701447fae0a
iddixx <ondarmurad2009@gmail.com>
2025-08-20 12:55:21 +0300

    (xmonad) made Gcr-promter floating by default

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -58,6 +58,7 @@ myManageHook = X.composeAll
     , X.className X.=? "copyq" X.--> (doFocus >> doCenterFloat)
     , X.className X.=? "mpv" X.--> (doFocus >> doCenterFloat)
     , X.className X.=? "gcr-prompter" X.--> (doFocus >> doCenterFloat)
+    , X.className X.=? "Gcr-prompter" X.--> (doFocus >> doCenterFloat)
     , X.title X.=? "Discord Updater" X.--> (doFocus >> doCenterFloat)
     , X.title X.=? "Settings" X.--> (doFocus >> doCenterFloat)
     , X.title X.=? "Friends List" X.--> (doFocus >> doCenterFloat)
