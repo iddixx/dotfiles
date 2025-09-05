39a8f00644070b38737ba77e82d4b012c2ec793f
iddixx <ondarmurad2009@gmail.com>
2025-09-05 23:57:41 +0300

    (polybar/xmobar) fixed brightness_ctl.sh

--- polybar/brightness_ctl.sh
+++ polybar/brightness_ctl.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 a='scale=2;' 
 a+=$(brightnessctl g)
-a+=' / 255 * 100'
+a+=' / '
+a+=$(brightnessctl m)
+a+=' * 100'
 calculations=$(bc -l <<< $a)
 echo "($calculations+0.5)/1" | bc 
 
