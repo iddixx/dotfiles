3be9d6df5e6873fbf7b7989cad6da56289d1f1b6
iddixx <ondarmurad2009@gmail.com>
2025-09-10 19:33:02 +0300

    (xmobar) rewrote brightness script in d, I hate shell scripts

---
+++ xmobar/brightness_ctl.d
@@ -0,0 +1,17 @@
+#!/bin/rdmd
+import std.algorithm;
+import std.process;
+import std.string;
+import std.getopt;
+import std.stdio;
+import std.conv;
+
+void main(string[] args)
+{
+    float current_brightness = to!float(filter!(a => a != '\n')(executeShell("brightnessctl g").output));
+    float max_brightness = to!float(filter!(a => a != '\n')(executeShell("brightnessctl m").output));
+    float percentage = (current_brightness / max_brightness * 100);
+
+    writeln(to!int(percentage));
+}
+
