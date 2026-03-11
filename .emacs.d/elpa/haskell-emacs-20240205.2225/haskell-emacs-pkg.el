e6069224d1e2b3017264bd51434aeb3045f8b06a
iddixx <ondarmurad2009@gmail.com>
2026-03-12 00:07:19 +0200

    (emacs) some emacs cache

---
+++ .emacs.d/elpa/haskell-emacs-20240205.2225/haskell-emacs-pkg.el
@@ -0,0 +1,8 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "haskell-emacs" "20240205.2225"
+  "Write emacs extensions in haskell."
+  ()
+  :url "https://github.com/knupfer/haskell-emacs"
+  :commit "777d5209f70229b53e4a60a09d1b714d233b0a7c"
+  :revdesc "777d5209f702"
+  :keywords '("haskell" "emacs" "ffi"))
