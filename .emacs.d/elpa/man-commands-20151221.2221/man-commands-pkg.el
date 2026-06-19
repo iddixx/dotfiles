0f0fc036173bb8250ab359165be4513c70b5ef25
iddixx <ondarmurad2009@gmail.com>
2026-06-19 18:54:43 +0300

    New portion of emacs poop

---
+++ .emacs.d/elpa/man-commands-20151221.2221/man-commands-pkg.el
@@ -0,0 +1,9 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "man-commands" "20151221.2221"
+  "Add interactive commands for every manpages installed in your computer."
+  '((cl-lib "0.5"))
+  :url "https://github.com/nflath/man-commands"
+  :commit "f4ba0c3790855d7544dff92d470d212f24de1d9d"
+  :revdesc "f4ba0c379085"
+  :authors '(("Nathaniel Flath" . "nflath@gmail.com"))
+  :maintainers '(("Nathaniel Flath" . "nflath@gmail.com")))
